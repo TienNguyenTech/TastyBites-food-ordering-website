c136e7df2a983e8cd0a31ac58374e9590a7be066
Ben Hetherington <bhet0004@student.monash.edu>
2024-04-18 01:07:39 +1000

    Fix styles on view admin users page

--- templates/Users/admin.php
+++ templates/Users/admin.php
@@ -6,12 +6,12 @@
 ?>
 <div class="users index content text-gray-800">
     <?= $this->Html->link(__('New User'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Users') ?></h3>
+    <h3><?= __('Admin Users') ?></h3>
     <div class="table-responsive">
-        <table>
+        <table class="table table-bordered">
             <thead>
             <tr>
-                <th><?= $this->Paginator->sort('user_id') ?></th>
+                <th><?= $this->Paginator->sort('user_id', 'User ID') ?></th>
                 <th><?= $this->Paginator->sort('email') ?></th>
                 <th><?= $this->Paginator->sort('user_type') ?></th>
                 <th class="actions"><?= __('Actions') ?></th>
@@ -24,23 +24,13 @@
                     <td><?= h($user->email) ?></td>
                     <td><?= h($user->user_type == 'admin' ? 'Admin' : 'Customer') ?></td>
                     <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $user->user_id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->user_id]) ?>
+<!--                        --><?php //= $this->Html->link(__('View'), ['action' => 'view', $user->user_id]) ?>
+<!--                        --><?php //= $this->Html->link(__('Edit'), ['action' => 'edit', $user->user_id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $user->user_id], ['confirm' => __('Are you sure you want to delete # {0}?', $user->user_id)]) ?>
                     </td>
                 </tr>
             <?php endforeach; ?>
             </tbody>
         </table>
-    </div>
-    <div class="paginator text-gray-800">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
 </div>
