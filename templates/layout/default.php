94d4cd90efc1bc1ef258f32f5acc870a6a4f8f70
Christina Doan <cdoa0004@student.monash.edu>
2024-04-17 21:51:41 +1000

    Remove user name
    removed user name default.php

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -107,7 +107,7 @@
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><p>Douglas McGee</p></span>
+<!--                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><p>Douglas McGee</p></span>-->
                             <?= $this->Html->image('undraw_profile.svg',['class'=>'img-profile rounded-circle'])?>
                         </a>
                         <!-- Dropdown - User Information -->
