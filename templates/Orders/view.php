428cf0f4ccd85ea3cbfa4a199bfb0b25fbeeb9e6
Ben Hetherington <bhet0004@student.monash.edu>
2024-04-18 00:26:06 +1000

    Fix styles on order pages

--- templates/Orders/view.php
+++ templates/Orders/view.php
@@ -4,7 +4,7 @@
  * @var \App\Model\Entity\Order $order
  */
 ?>
-<div class="row">
+<div class="row text-gray-800">
 <!--    <aside class="column" >-->
 <!--        <div class="side-nav">-->
 <!--            <h4 class="heading">--><?php //= __('Actions') ?><!--</h4>-->
@@ -20,46 +20,48 @@
 <!--            <h3>--><?php //= h( $order->order_id) ?><!--</h3>-->
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <tr>
-                    <th><?= __('Customer') ?></th>
-                    <td><?= $order->hasValue('customer') ? $this->Html->link($order->customer->customer_fname, ['controller' => 'Customers', 'action' => 'view', $order->customer->customer_id]) : '' ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Order Id') ?></th>
+                    <th><?= __('Order ID') ?></th>
                     <td><?= $this->Number->format($order->order_id) ?></td>
                 </tr>
                 <tr>
-                    <th><?= __('Order Total') ?></th>
-                    <td><?= $this->Number->format($order->order_total) ?></td>
+                    <th><?= __('Customer') ?></th>
+                    <td><?= $order->hasValue('customer') ? $this->Html->link($order->customer->customer_fname . ' ' . $order->customer->customer_lname, ['controller' => 'Customers', 'action' => 'view', $order->customer->customer_id]) : '' ?></td>
                 </tr>
                 <tr>
                     <th><?= __('Order Datetime') ?></th>
                     <td><?= h($order->order_datetime) ?></td>
                 </tr>
+                <tr>
+                    <th><?= __('Order Total') ?></th>
+                    <td><?= $this->Number->currency($order->order_total) ?></td>
+                </tr>
             </table>
             <div class="related">
-                <h4><?= __('Related Menuitems') ?></h4>
+                <h4><?= __('Item in Order') ?></h4>
                 <?php if (!empty($order->menuitems)) : ?>
                 <div class="table-responsive">
                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                         <tr>
                             <th><?= __('Menuitem Id') ?></th>
-                            <th><?= __('Menuitem Desc') ?></th>
-                            <th><?= __('Menuitem Price') ?></th>
-                            <th><?= __('Menuitem Rating') ?></th>
                             <th><?= __('Menuitem Name') ?></th>
+                            <th><?= __('Menuitem Price') ?></th>
+                            <th><?= __('Menuitem Desc') ?></th>
+
+
                             <th class="actions"><?= __('Actions') ?></th>
                         </tr>
                         <?php foreach ($order->menuitems as $menuitem) : ?>
                         <tr>
                             <td><?= h($menuitem->menuitem_id) ?></td>
-                            <td><?= h($menuitem->menuitem_desc) ?></td>
-                            <td><?= h($menuitem->menuitem_price) ?></td>
-                            <td><?= h($menuitem->menuitem_rating) ?></td>
                             <td><?= h($menuitem->menuitem_name) ?></td>
+                            <td><?= $this->Number->currency($menuitem->menuitem_price) ?></td>
+                            <td><?= h($menuitem->menuitem_desc) ?></td>
+
+
                             <td class="actions">
                                 <?= $this->Html->link(__('View'), ['controller' => 'Menuitems', 'action' => 'view', $menuitem->menuitem_id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'Menuitems', 'action' => 'edit', $menuitem->menuitem_id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'Menuitems', 'action' => 'delete', $menuitem->menuitem_id], ['confirm' => __('Are you sure you want to delete # {0}?', $menuitem->menuitem_id)]) ?>
+<!--                                --><?php //= $this->Html->link(__('Edit'), ['controller' => 'Menuitems', 'action' => 'edit', $menuitem->menuitem_id]) ?>
+<!--                                --><?php //= $this->Form->postLink(__('Delete'), ['controller' => 'Menuitems', 'action' => 'delete', $menuitem->menuitem_id], ['confirm' => __('Are you sure you want to delete # {0}?', $menuitem->menuitem_id)]) ?>
                             </td>
                         </tr>
                         <?php endforeach; ?>
