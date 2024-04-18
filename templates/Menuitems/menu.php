66ad51a43ceb87ce2b516168e880ba08f5d96b3f
tngu0394 <tngu0394@student.monash.edu>
2024-04-18 15:17:24 +1000

    Menu Template update

--- templates/Menuitems/menu.php
+++ templates/Menuitems/menu.php
@@ -5,40 +5,38 @@
  */
 ?>
 
-
-
-<div class="menuitems index content text-gray-800">
-    <?= $this->Html->link(__('New Menuitem'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3 class="text-gray-800">Menu Items</h3>
-
-    <?= $this->Form->create(null, [
-        'url' => [
-            'action' => 'search'
-        ]
-    ]) ?>
-
-
-<!-- File: src/Template/Menu/index.ctp -->
-<div class="menu-container">
-    <?php foreach ($menuitems as $menuitem): ?>
-        <div class="card" style="width: 18rem; margin: 10px;">
-            <img src="../webroot/img/momo_background.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title"><?= $menuitem->menuitem_name?></h5>
-                <h5><?= $this->Number->currency($menuitem->menuitem_price) ?></h5>
-                <p class="card-text text-gray-800"><?= $menuitem->menuitem_desc ?></p>
-            </div>
-            <div class="card-body">
-                <a href="#" class="card-link"><button>Add to cart</button></a>
-            </div>
+<div class="container my-4">
+    <div class="row justify-content-between align-items-center mb-3">
+        <div class="col">
+            <h3 class="text-gray-800 mb-0">Menu Items</h3>
         </div>
-    <?php endforeach; ?>
+        <div class="col-auto">
+            <?= $this->Html->link(__('New Menuitem'), ['action' => 'add'], ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <?php foreach ($menuitems as $menuitem): ?>
+            <div class="col mb-4">
+                <div class="card h-100 shadow">
+                    <img src="../webroot/img/momo_background.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $menuitem->menuitem_name ?></h5>
+                        <h5><?= $this->Number->currency($menuitem->menuitem_price) ?></h5>
+                        <p class="card-text"><?= $menuitem->menuitem_desc ?></p>
+                    </div>
+                    <div class="card-footer bg-white border-top-0 text-center">
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 
-
+<!-- JavaScript for DataTables -->
 <script>
     $(document).ready(function () {
         $('#dataTable').DataTable();
     });
 </script>
-</div>
