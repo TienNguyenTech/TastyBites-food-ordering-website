2990780531b472a3e2694c404cf049a40d618848
Ben Hetherington <bhet0004@student.monash.edu>
2024-04-18 12:50:33 +1000

    Format add enquiry form
    Still needs some work

--- templates/Enquirys/add.php
+++ templates/Enquirys/add.php
@@ -11,12 +11,12 @@
             <fieldset>
                 <legend><?= __('Add Enquiry') ?></legend>
                 <?php
-                    echo $this->Form->control('enquiry_name');
-                    echo $this->Form->control('enquiry_email');
-                    echo $this->Form->control('enquiry_message');
+                    echo $this->Form->control('enquiry_name', ['label' => 'Name', 'class' => 'form-control']);
+                    echo $this->Form->control('enquiry_email', ['label' => 'Email', 'class' => 'form-control', 'type' => 'email']);
+                    echo $this->Form->control('enquiry_message', ['label' => 'Message', 'class' => 'form-control', 'type' => 'textarea']);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-primary']) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
