330926ac60ebb3c0243ff2040d5227418ce5e846
tngu0394 <tngu0394@student.monash.edu>
2024-04-18 16:11:08 +1000

    Customer layout update

--- templates/layout/customer.php
+++ templates/layout/customer.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -12,7 +13,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles2.css" rel="stylesheet" />
-   
+
 
     <!-- Custom fonts for this template-->
     <?= $this->Html->css('/vendor/fontawesome-free/css/all.min.css') ?>
@@ -29,77 +30,120 @@
 
     <?= $this->Html->script('/vendor/jquery/jquery.min.js') ?>
 </head>
+
 <body>
-<!-- Responsive navbar-->
-<style>
-    /* Override Bootstrap's primary color */
-    .navbar-tea {
-        background-color: #273d4f;
-    }
+    <!-- Responsive navbar-->
+    <style>
+        /* Override Bootstrap's primary color */
+        .navbar-tea {
+            background-color: #273d4f;
+        }
 
-    .fire-text {
-        color: #fff5f1; /* Color of fire text */
-    }
-    .fire-text:hover{
-        color: #cb4c46;
-    }
-    .Big-Stuff {
-        color: #fff5f1; /* Color of active page */
-        font-size: 25px;
-    }
-    .Big-Stuff:hover{
-        color: #cb4c46;
-    }
-    .active-home {
-        color:#6fb89c ;
-    }
-    .active-home:hover {
-        color: #cb4c46;
-    }
-</style>
+        .fire-text {
+            color: #fff5f1;
+            /* Color of fire text */
+        }
 
-<nav class="navbar navbar-expand-lg navbar-tea">
-    <div class="container-fluid">
-        <a class="navbar-brand text-white" href="#"><b>Tasty Bites Kitchen</b></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0"> <!-- Changed 'ms-auto' to 'me-auto' -->
-                <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <?= $this->Html->link('Menu', ['controller' => 'Menuitems', 'action' => 'menu'], ['class' => 'nav-link text-white']) ?>
-                </li>
-                <li class="nav-item">
-                    <?= $this->Html->link('Enquiry', ['controller' => 'Enquirys', 'action' => 'add'], ['class' => 'nav-link text-white']) ?>
-                </li>
-                <?php if (!$this->Identity->isLoggedIn()): ?>
-                <li class="nav-item">
-                    <?= $this->Html->link('Log in', ['controller' => 'Auth', 'action' => 'login'], ['class' => 'nav-link text-white']) ?>
-                </li>
-                <?php endif; ?>
-                <?php if ($this->Identity->isLoggedIn()): ?>
-                <li class="nav-item">
-                    <?= $this->Html->link('Dashboard', ['controller' => 'Dashboard', 'action' => 'index'], ['class' => 'nav-link text-white']) ?>
-                </li>
-                </ul> <!-- Close the 'ul' tag here -->
-                <ul class="navbar-nav"> <!-- New 'ul' tag for the right-aligned items -->
+        .fire-text:hover {
+            color: #cb4c46;
+        }
+
+        .Big-Stuff {
+            color: #fff5f1;
+            /* Color of active page */
+            font-size: 25px;
+        }
+
+        .Big-Stuff:hover {
+            color: #cb4c46;
+        }
+
+        .active-home {
+            color: #6fb89c;
+        }
+
+        .active-home:hover {
+            color: #cb4c46;
+        }
+    </style>
+
+    <nav class="navbar navbar-expand-lg navbar-tea">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#"><b>Tasty Bites Kitchen</b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0"> <!-- Changed 'ms-auto' to 'me-auto' -->
                     <li class="nav-item">
-                        <form action="<?= $this->Url->build(['controller' => 'Auth', 'action' => 'logout']) ?>" method="post">
-                            <button type="submit" class="btn btn-outline-light">Logout</button>
-                        </form>
+                        <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
                     </li>
-                </ul>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <?= $this->Html->link('Menu', ['controller' => 'Menuitems', 'action' => 'menu'], ['class' => 'nav-link text-white']) ?>
+                    </li>
+                    <li class="nav-item">
+                        <?= $this->Html->link('Enquiry', ['controller' => 'Enquirys', 'action' => 'add'], ['class' => 'nav-link text-white']) ?>
+                    </li>
+                    <?php if (!$this->Identity->isLoggedIn()): ?>
+                        <li class="nav-item">
+                            <?= $this->Html->link('Log in', ['controller' => 'Auth', 'action' => 'login'], ['class' => 'nav-link text-white']) ?>
+                        </li>
+                    <?php endif; ?>
+                    <?php if ($this->Identity->isLoggedIn()): ?>
+                        <li class="nav-item">
+                            <?= $this->Html->link('Dashboard', ['controller' => 'Dashboard', 'action' => 'index'], ['class' => 'nav-link text-white']) ?>
+                        </li>
+                    </ul> <!-- Close the 'ul' tag here -->
+                    <ul class="navbar-nav"> <!-- New 'ul' tag for the right-aligned items -->
+                        <li class="nav-item">
+                            <form action="<?= $this->Url->build(['controller' => 'Auth', 'action' => 'logout']) ?>"
+                                method="post">
+                                <button type="submit" class="btn btn-outline-light">Logout</button>
+                            </form>
+                        </li>
+                    </ul>
                 <?php endif; ?>
-            </ul>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    <!-- Site footer -->
+    <footer class="bg-dark text-white py-4 fixed-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About Us</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Contact Us</h5>
+                <p>123 Main Street, City, Country</p>
+                <p>Email: info@example.com</p>
+                <p>Phone: +123 456 7890</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#"><i class="bi bi-facebook"></i> Facebook</a></li>
+                    <li><a href="#"><i class="bi bi-twitter"></i> Twitter</a></li>
+                    <li><a href="#"><i class="bi bi-instagram"></i> Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr class="bg-light">
+        <div class="text-center">
+            <p>&copy; <?= date('Y') ?> Tasty Bites Kitchen. All rights reserved.</p>
         </div>
     </div>
-</nav>
+</footer>
 
 
-<?= $this->fetch('content') ?>
+
+    <?= $this->fetch('content') ?>
