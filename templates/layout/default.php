69fe3038da714a7bfcfa6bf12ec616a0a14247cc
Yiming Huang <yhua0196@student.monash.edu>
2024-04-18 13:24:52 +1000

    delete the duplicate search bar and edit the header of dashboard
    now only one search bar in menu and remove the word "Dashboard" at the header. Replace the profile img with just "logout" and add a message pop up when logout

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -49,12 +49,12 @@
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
-            <a class="nav-link" href="<?= $this->Url->build(['controller'=>'Pages','action'=>'display','home']) ?>">
-                <i class="fas fa-fw fa-home"></i>
-                <span>Homepage</span></a>
             <a class="nav-link" href="<?= $this->Url->build(['controller'=>'Dashboard','action'=>'index']) ?>">
                 <i class="fas fa-fw fa-cog"></i>
                 <span>Dashboard</span></a>
+            <a class="nav-link" href="<?= $this->Url->build(['controller'=>'Pages','action'=>'display','home']) ?>">
+                <i class="fas fa-fw fa-home"></i>
+                <span>Homepage</span></a>
             <a class="nav-link" href="<?= $this->Url->build(['controller'=>'Menuitems','action'=>'index']) ?>">
                 <i class="fas fa-fw fa-birthday-cake"></i>
                 <span>Menu</span></a>
@@ -100,34 +100,60 @@
             <!-- Topbar -->
             <nav class="navbar navbar-expand navbar-light navbar-colour topbar mb-4 static-top shadow">
 
-                <div class="header-left navbar-colour">
-                            <div class="dashboard_bar">
-                                Dashboard
-                            </div>
-                </div>
+<!--                <div class="header-left navbar-colour">-->
+<!--                            <div class="dashboard_bar">-->
+<!--                                Dashboard-->
+<!--                            </div>-->
+<!--                </div>-->
                 <!-- Sidebar Toggle (Topbar) -->
                 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                     <i class="fa fa-bars"></i>
                 </button>
 
                 <!-- Topbar Navbar -->
+<!--                <ul class="navbar-nav ml-auto">-->
+<!---->
+<!--                    <li class="nav-item dropdown no-arrow">-->
+<!--                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"-->
+<!--                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                            --><?php //= $this->Html->image('undraw_profile.svg',['class'=>'img-profile rounded-circle'])?>
+<!--                        </a>-->
+                       <!-- Dropdown - User Information -->
+<!--                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">-->
+<!--                            --><?php //if ($this->Identity->isLoggedIn()): ?>
+<!--                                --><?php //= $this->Html->link('Logout', ['controller' => 'Auth', 'action' => 'logout'], ['class' => 'dropdown-item']) ?>
+<!--                            --><?php //endif; ?>
+<!--                        </div>-->
+<!--                    </li>-->
+<!---->
+<!--                </ul>-->
+
                 <ul class="navbar-nav ml-auto">
-
-                    <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-<!--                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><p>Douglas McGee</p></span>-->
-                            <?= $this->Html->image('undraw_profile.svg',['class'=>'img-profile rounded-circle'])?>
-                        </a>
-                        <!-- Dropdown - User Information -->
-                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                            <?php if ($this->Identity->isLoggedIn()): ?>
-                                <?= $this->Html->link('Logout', ['controller' => 'Auth', 'action' => 'logout'], ['class' => 'dropdown-item']) ?>
-                            <?php endif; ?>
-                        </div>
+                    <li class="nav-item">
+                        <?php if ($this->Identity->isLoggedIn()): ?>
+                            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+                        <?php endif; ?>
                     </li>
-
                 </ul>
+
+                <!-- Logout Confirmation Modal -->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" style="color: black;" id="exampleModalLabel">Ready to Leave?</h5>
+                                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span>
+                                </button>
+                            </div>
+                            <div class="modal-body" style="color: black;">Are you sure you want to logout?</div>
+                            <div class="modal-footer">
+                                <button class="btn btn-secondary" type="button" data-dismiss="modal">No</button>
+                                <?= $this->Html->link('Yes', ['controller' => 'Auth', 'action' => 'logout'], ['class' => 'btn btn-primary']) ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             </nav>
             <!-- End of Topbar -->
