79c2b5ae16c7a6d8412dac068c860e069b63cd2b
Ben Hetherington <bhet0004@student.monash.edu>
2024-04-19 21:57:47 +1000

    Fix form

--- templates/Enquirys/add.php
+++ templates/Enquirys/add.php
@@ -7,9 +7,9 @@
 <div class="row">
     <div class="column column-80">
         <div class="enquirys form content">
-            <?= $this->Form->create($enquiry) ?>
+            <?= $this->Form->create($enquiry, ['style' => 'margin-left: 20px']) ?>
             <fieldset>
-                <legend><?= __('Add Enquiry') ?></legend>
+                <legend><?= __('Contact Us') ?></legend>
                 <?php
                     echo $this->Form->control('enquiry_name', ['label' => 'Name', 'class' => 'form-control']);
                     echo $this->Form->control('enquiry_email', ['label' => 'Email', 'class' => 'form-control', 'type' => 'email']);
