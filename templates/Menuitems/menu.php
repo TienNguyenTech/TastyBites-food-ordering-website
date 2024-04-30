03f5b1955607ec8e8b72629b532c6d822dc9f3bb
tngu0394 <tngu0394@student.monash.edu>
2024-04-30 10:57:42 +1000

    cart icon adding

--- templates/Menuitems/menu.php
+++ templates/Menuitems/menu.php
@@ -82,48 +82,92 @@
             margin: 0;
             /* No extra margin on any side */
         }
+
+        .container1 {
+            width: 900px;
+            max-width: 90%;
+            margin: auto;
+            text-align: center;
+            padding-top: 10px;
+        }
+
+        svg {
+            width: 30px;
+        }
+
+        header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 20px 0;
+        }
+
+        header .icon-cart1 {
+            position: relative;
+        }
+
+        header .icon-cart1 span {
+            display: flex;
+            width: 30px;
+            height: 30px;
+            background-color: red;
+            justify-content: center;
+            align-items: center;
+            color: white;
+            border-radius: 50%;
+            position: absolute;
+            top: 50%;
+            right: -20px;
+        }
     </style>
 </head>
 
 <body>
     <div class="my-4" style="margin: 0 6rem;">
         <div class="row justify-content-between align-items-center mb-3">
-            <div class="col">
-                <h4 class="text-gray-800 mb-0"><b>Tasty Bites Menu</b></h4>
-            </div>
 
-            <div class="col-auto">
-                <!-- Add New Item button -->
-                <?= $this->Html->link(__('Add New Item'), ['action' => 'add'], ['class' => 'btn btn-primary']) ?>
-            </div>
-            <div class="icon-cart">
-                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                    width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312" />
-                </svg>
-                <span>0</span>
-            </div>
 
-            <div class="row row-cols-1 row-cols-md-5 g-4">
-                <?php foreach ($menuitems as $menuitem): ?>
+            <div class="container1">
+                <header>
                     <div class="col">
-                        <div class="card shadow">
-                            <?= $this->Html->image('menu/' . $menuitem->menuitem_image, ['alt' => $menuitem->menuitem_name, 'class' => 'card-img-top']) ?>
-                            <div class="card-body">
-                                <h5 class="card-title"><?= $menuitem->menuitem_name ?></h5>
-                                <p class="card-text"><?= $menuitem->menuitem_desc ?></p> <!-- Description section -->
-                                <span class="text-muted"><?= $this->Number->currency($menuitem->menuitem_price) ?></span>
-                                <!-- Price aligned left, just below description -->
-                            </div>
-                            <div class="card-footer">
-                                <!-- Add to Cart button, centered in the footer -->
-                                <?= $this->Html->link(__('Add to Cart'), ['controller' => 'OrdersMenuitems', 'action' => 'cart', $menuitem->id, 1], ['class' => 'btn btn-primary']) ?>
+                        <h4 class="text-gray-800 mb-0"><b>Tasty Bites Menu</b></h4>
+                    </div>
+                    <div class="col-auto">
+                        <!-- Add New Item button -->
+                        <?= $this->Html->link(__('Add New Item'), ['action' => 'add'], ['class' => 'btn btn-primary']) ?>
+                    </div>
+                    <div class="icon-cart1">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312" />
+                        </svg>
+                        <span>0</span>
+                    </div>
+            </div>
+            <header>
+
+
+                <div class="row row-cols-1 row-cols-md-5 g-4">
+                    <?php foreach ($menuitems as $menuitem): ?>
+                        <div class="col">
+                            <div class="card shadow">
+                                <?= $this->Html->image('menu/' . $menuitem->menuitem_image, ['alt' => $menuitem->menuitem_name, 'class' => 'card-img-top']) ?>
+                                <div class="card-body">
+                                    <h5 class="card-title"><?= $menuitem->menuitem_name ?></h5>
+                                    <p class="card-text"><?= $menuitem->menuitem_desc ?></p> <!-- Description section -->
+                                    <span
+                                        class="text-muted"><?= $this->Number->currency($menuitem->menuitem_price) ?></span>
+                                    <!-- Price aligned left, just below description -->
+                                </div>
+                                <div class="card-footer">
+                                    <!-- Add to Cart button, centered in the footer -->
+                                    <?= $this->Html->link(__('Add to Cart'), ['controller' => 'OrdersMenuitems', 'action' => 'cart', $menuitem->id, 1], ['class' => 'btn btn-primary']) ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+                    <?php endforeach; ?>
+                </div>
         </div>
 </body>
 
