77fb861f96b5cc8fda88dde6ee70d36f56cdf106
Ben Hetherington <bhet0004@student.monash.edu>
2024-05-02 02:03:26 +1000

    Hide ID fields

--- templates/Users/admin.php
+++ templates/Users/admin.php
@@ -11,22 +11,18 @@
         <table class="table table-bordered">
             <thead>
             <tr>
-                <th><?= $this->Paginator->sort('user_id', 'Admin ID') ?></th>
                 <th><?= $this->Paginator->sort('first_name') ?></th>
                 <th><?= $this->Paginator->sort('last_name') ?></th>
                 <th><?= $this->Paginator->sort('email') ?></th>
-                <th><?= $this->Paginator->sort('user_type') ?></th>
                 <th class="actions"><?= __('Actions') ?></th>
             </tr>
             </thead>
             <tbody>
             <?php foreach ($users as $user): ?>
                 <tr>
-                    <td><?= $this->Number->format($user->user_id) ?></td>
                     <td><?= h($user->first_name) ?></td>
                     <td><?= h($user->last_name) ?></td>
                     <td><?= h($user->email) ?></td>
-                    <td><?= h($user->user_type == 'admin' ? 'Admin' : 'Customer') ?></td>
                     <td class="actions">
 <!--                        --><?php //= $this->Html->link(__('View'), ['action' => 'view', $user->user_id]) ?>
 <!--                        --><?php //= $this->Html->link(__('Edit'), ['action' => 'edit', $user->user_id]) ?>
