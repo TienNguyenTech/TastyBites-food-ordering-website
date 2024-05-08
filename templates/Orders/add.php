7516b2253cbdceb803994491525e54c7763e069c
Christina Doan <cdoa0004@student.monash.edu>
2024-05-08 23:26:19 +1000

    can only input number for customer phone
    add.php

--- templates/Orders/add.php
+++ templates/Orders/add.php
@@ -18,7 +18,15 @@
 <?php ////= $this->Form->button(__('Submit'),['class'=>'btn btn-primary']) ?>
 <?php ////= $this->Form->end() ?>
 
-
+<script>
+    function isNumeric(evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode;
+        if (charCode >= 48 && charCode <= 57) {
+            return true;
+        }
+        return false;
+    }
+</script>
 <div class="row">
     <div class="enquirys form container" style="padding: 20px; background-color: #e8e8e8; margin-top: 20px; border-radius: 10px">
         <?= $this->Form->create($order) ?>
@@ -27,7 +35,11 @@
             <?php
             echo $this->Form->control('customer_name', ['label' => 'Full Name', 'class' => 'form-control']);
             echo $this->Form->control('customer_email', ['label' => 'Contact Email', 'class' => 'form-control']);
-            echo $this->Form->control('customer_phone', ['label' => 'Contact Phone', 'class' => 'form-control']);
+            echo $this->Form->control('customer_phone', ['label' => 'Contact Phone',
+                'required' => true,
+                'class' => 'form-control',
+                'onkeypress' => 'return isNumeric(event)',
+            ]);
             echo $this->Form->control('menuitems._ids', ['options' => $menuitems, 'class' => 'form-control', 'required']);
 
             ?>
