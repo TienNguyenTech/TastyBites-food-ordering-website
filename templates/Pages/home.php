9a44ac920a07dd8e9ce93b852013eded57903bde
Surya Vamshi Mukunda Rao <srao0005@student.monash.edu>
2024-05-08 12:50:25 +1000

    Fixed carousel after it broke

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -501,29 +501,34 @@ $this->disableAutoLayout();
                                 <!--                                <img src="--><?php //= $this->ContentBlock->image('shown-image-1'); ?><!--" class="d-block w-100" alt="Image 1">-->
                                 <?= $this->ContentBlock->image('shown-image-1'); ?>
                             </div>
-                            <!-- Add other carousel items here -->
+                            <div class="carousel-item">
+                                <!--                                <img src="webroot/img/LMAO3.jpg" class="d-block w-100" alt="Image 3">-->
+                                <?= $this->ContentBlock->image('shown-image-2'); ?>
+                            </div>
+                            <div class="carousel-item">
+                                <!--                                <img src="webroot/img/LMAO4.jpg" class="d-block w-100" alt="Image 4">-->
+                                <?= $this->ContentBlock->image('shown-image-3'); ?>
+                            </div>
+                            <div class="carousel-item">
+                                <!--                                <img src="webroot/img/LMAO5.jpg" class="d-block w-100" alt="Image 5">-->
+                                <?= $this->ContentBlock->image('shown-image-4'); ?>
+                            </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleSlidesOnly"
-                                data-bs-slide="prev">
+                            data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
                         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleSlidesOnly"
-                                data-bs-slide="next">
+                            data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
-                    </div>
-
-                    <!-- Google Maps Location Widget -->
-                    <div class="google-maps-widget">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.756198102105!2d145.1307322147605!3d-37.90782117972163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDXCsDM5JzA5LjYiUyAxNDXCsDIzJzU3LjkiRQ!5e0!3m2!1sen!2suk!4v1630634060110!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
 
     <!-- Features section-->
     <style>
