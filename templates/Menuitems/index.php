32082e9251788863837d1dd1eb3e405b90ff9c29
tngu0394 <tngu0394@student.monash.edu>
2024-05-13 17:51:44 +1000

    Delete button

--- templates/Menuitems/index.php
+++ templates/Menuitems/index.php
@@ -10,50 +10,48 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['bl
 <!-- Bootstrap CSS, this is an experiment-->
 <!--<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">-->
 
+
 <div class="menuitems index content text-gray-800">
-    <?= $this->Html->link(__('New Menuitem'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3 class="text-gray-800">Menu Items</h3>
+<?= $this->Html->link(__('New Menuitem'), ['action' => 'add'], ['class' => 'button float-right']) ?>
+<h3 class="text-gray-800">Menu Items</h3>
 
-    <div class="table-responsive">
-        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-            <thead>
+<div class="table-responsive">
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th class="actions"><?= __('Actions') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($menuitems as $menuitem): ?>
                 <tr>
-
-                    <th>Name</th>
-                    <!--                    <th>--><?php //= h('menuitem_image') ?><!--</th>-->
-                    <th>Image</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                    <!--                    <th>--><?php //= h('menuitem_rating') ?><!--</th>-->
-                    <th class="actions"><?= __('Actions') ?></th>
+                    <td><?= h($menuitem->menuitem_name) ?></td>
+                    <td><?= $this->Html->image('menu/' . $menuitem->menuitem_image, ['alt' => $menuitem->menuitem_name, 'style' => 'max-width: 100px;']) ?></td>
+                    <td><?= h($menuitem->menuitem_desc) ?></td>
+                    <td><?= $this->Number->currency($menuitem->menuitem_price) ?></td>
+                    <td class="actions">
+                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $menuitem->menuitem_id]) ?>
+                        <?= $this->Form->postLink(
+                            __('Delete'),
+                            ['action' => 'delete', $menuitem->menuitem_id],
+                            [
+                                'confirm' => __('Are you sure you want to delete {0}?', $menuitem->menuitem_name),
+                                'class' => 'btn btn-danger delete-menu-item-btn',
+                                'id' => 'deleteMenuItemButton' // Add an ID for easy access in JavaScript
+                            ]
+                        ) ?>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($menuitems as $menuitem): ?>
-                    <tr>
-                        <td><?= h($menuitem->menuitem_name) ?></td>
-                        <td><?= $this->Html->image('menu/' . $menuitem->menuitem_image, ['alt' => $menuitem->menuitem_name, 'style' => 'max-width: 100px;']) ?>
-                        </td>
-                        <td><?= h($menuitem->menuitem_desc) ?></td>
-                        <td><?= $this->Number->currency($menuitem->menuitem_price) ?></td>
-                        <td class="actions">
-                            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $menuitem->menuitem_id]) ?>
-                            <?= $this->Form->postLink(
-                                __('Delete'),
-                                ['action' => 'delete', $menuitem->menuitem_id],
-                                [
-                                    'confirm' => __('Are you sure you want to delete {0}?', $menuitem->menuitem_name),
-                                    'class' => 'btn btn-danger delete-menu-item-btn',
-                                    'id' => 'deleteMenuItemButton' // Add an ID for easy access in JavaScript
-                                ]
-                            ) ?>
-                        </td>
-                    </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
 
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+
     <script>
         $(document).ready(function () {
             $('#dataTable').DataTable();
