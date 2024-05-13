9caf10e39b4236c667863e7079b7cd3c8bf1f69f
tngu0394 <tngu0394@student.monash.edu>
2024-05-13 16:23:22 +1000

    Edit Menu Admin | Description Length Restriction 255

--- templates/Menuitems/edit.php
+++ templates/Menuitems/edit.php
@@ -9,7 +9,7 @@
 <?= $this->Form->create($menuitem, ['type' => 'file', 'class' => 'text-gray-800']) ?>
 <?php
 echo $this->Form->control('menuitem_name', ['label' => 'Name', 'class' => 'form-control']);
-echo $this->Form->control('menuitem_desc', ['label' => 'Description', 'class' => 'form-control', 'type' => 'textarea']);
+echo $this->Form->control('menuitem_desc', ['label' => 'Description', 'class' => 'form-control', 'maxlength' => '255', 'type' => 'textarea']);
 echo $this->Form->control('menuitem_price', ['label' => 'Price', 'class' => 'form-control', 'min' => '0', 'style' => 'margin-bottom: 10px']);
 ?>
 <?= $this->Form->button(__('Submit'),['class'=>'btn btn-primary']) ?>
