fc643e1a6c1e800e51286f18d702193906ec0430
tngu0394 <tngu0394@student.monash.edu>
2024-05-16 01:32:35 +1000

    lOGIN COLOR CHANGE

--- templates/Auth/login.php
+++ templates/Auth/login.php
@@ -10,54 +10,69 @@ $debug = Configure::read('debug');
 $this->layout = 'login';
 $this->assign('title', 'Login');
 ?>
-<div class="container login">
-    <div class="row">
-        <div class="column column-50 column-offset-25">
-            <h1 class="page-title">Tasty Bites Kitchen</h1>
-            <div class="users form content">
 
-                <?= $this->Form->create() ?>
+<html>
 
-                <fieldset>
+<body>
+    <style>
+        body{
+            background-color: #22408c;
+        }
+        .centered-button {
+            background-color: #22408c;
+        }
+    </style>
+    <div class="container login">
+        <div class="row">
+            <div class="column column-50 column-offset-25">
+                <h1 class="page-title">Tasty Bites Kitchen</h1>
+                <div class="users form content">
 
-                    <legend style="font-size: 24px;">Login</legend>
+                    <?= $this->Form->create() ?>
 
-                    <?= $this->Flash->render() ?>
+                    <fieldset>
 
-                    <?php
-                    /*
-                     * NOTE: regarding 'value' config in the login page form controls
-                     * In this demo the email and password fields will be filled by demo account
-                     * credentials when debug mode is on. You should NOT do that in your production
-                     * systems. Also it's a good practice to clear (set password value to empty)
-                     * in the view so when an error occurred with form validation, the password
-                     * values are always cleared.
-                     */
-                    echo $this->Form->control('email', [
-                        'type' => 'email',
-                        'required' => true,
-                        'autofocus' => true,
-//                        'value' => $debug ? "test@example.com" : "",
-                    ]);
-                    echo $this->Form->control('password', [
-                        'type' => 'password',
-                        'required' => true,
-//                        'value' => $debug ? '1234' : '',
-                    ]);
-                    ?>
-<!--                    --><?php //= $this->Html->link('Forgot password?', ['controller' => 'Auth', 'action' => 'forgetPassword']) ?>
-                </fieldset>
+                        <legend style="font-size: 24px;">Login</legend>
 
-<!--                --><?php //= $this->Form->button('Login') ?>
-                <div class="input-wrapper">
-                    <?= $this->Form->button('Login', ['class' => 'centered-button']) ?>
-                <?= $this->Form->end() ?>
+                        <?= $this->Flash->render() ?>
 
-                <hr class="hr-between-buttons">
+                        <?php
+                        /*
+                         * NOTE: regarding 'value' config in the login page form controls
+                         * In this demo the email and password fields will be filled by demo account
+                         * credentials when debug mode is on. You should NOT do that in your production
+                         * systems. Also it's a good practice to clear (set password value to empty)
+                         * in the view so when an error occurred with form validation, the password
+                         * values are always cleared.
+                         */
+                        echo $this->Form->control('email', [
+                            'type' => 'email',
+                            'required' => true,
+                            'autofocus' => true,
+                            //                        'value' => $debug ? "test@example.com" : "",
+                        ]);
+                        echo $this->Form->control('password', [
+                            'type' => 'password',
+                            'required' => true,
+                            //                        'value' => $debug ? '1234' : '',
+                        ]);
+                        ?>
+                        <!--                    --><?php //= $this->Html->link('Forgot password?', ['controller' => 'Auth', 'action' => 'forgetPassword']) ?>
+                    </fieldset>
 
-<!--                --><?php //= $this->Html->link("Don't have an account? Sign Up", ['controller' => 'Auth', 'action' => 'register'], ['class' => 'button button-clear']) ?>
-                <?= $this->Html->link('Go to Homepage', '/', ['class' => 'button button-clear']) ?>
+                    <!--                --><?php //= $this->Form->button('Login') ?>
+                    <div class="input-wrapper">
+                        <?= $this->Form->button('Login', ['class' => 'centered-button']) ?>
+                        <?= $this->Form->end() ?>
+
+                        <hr class="hr-between-buttons">
+
+                        <!--                --><?php //= $this->Html->link("Don't have an account? Sign Up", ['controller' => 'Auth', 'action' => 'register'], ['class' => 'button button-clear']) ?>
+                        <?= $this->Html->link('Go to Homepage', '/', ['class' => 'button button-clear']) ?>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+</body>
+
+</html>
