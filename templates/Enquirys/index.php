a87829ae5f8e1a723a6176c16dc766561316da9e
Ben Hetherington <bhet0004@student.monash.edu>
2024-05-16 01:26:47 +1000

    Word wrap

--- templates/Enquirys/index.php
+++ templates/Enquirys/index.php
@@ -8,12 +8,8 @@ echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css',['block
 echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js',['block'=>true]);
 echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js',['block'=>true]);
 ?>
-<div class="orders index content text-gray-800">
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"><?= __('Enquiries') ?></h1>
-        <!--        <a href="--><?php //= $this->Url->build(['action' => 'add'])?><!--" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i-->
-        <!--                class="fas fa-plus fa-sm text-white-50"></i> New Order</a>-->
-    </div>
+<div class="enquirys index content text-gray-800">
+    <h3 class="h3 mb-0 text-gray-800"><?= __('Enquiries') ?></h3>
     <div class="table-responsive">
         <div>
             <table class="table table-bordered" id="dataTable">
@@ -30,9 +26,8 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js',['blo
                     <tr>
                         <td><?= h($enquiry->enquiry_name) ?></td>
                         <td><?= $this->Html->link($enquiry->enquiry_email, "mailto:" . $enquiry->enquiry_email) ?></td>
-                        <td><?= h($enquiry->enquiry_message) ?></td>
+                        <td style="word-break : break-all;"><?= h($enquiry->enquiry_message) ?></td>
                         <td class="actions">
-<!--                            --><?php //= $this->Html->link(__('View'), ['action' => 'view', $enquiry->enquiry_id]) ?>
                             <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $enquiry->enquiry_id], ['class' => 'btn btn-danger','confirm' => __('Are you sure you want to delete this enquiry from# {0}?', $enquiry->enquiry_email)]) ?>
                         </td>
                     </tr>
