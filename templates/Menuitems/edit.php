b7c8c0c87beb5570cb0cae92dd00cc355fdb5b87
Yiming Huang <yhua0196@student.monash.edu>
2024-05-22 18:37:33 +1000

    add asterisk for edit menu item page

--- templates/Menuitems/edit.php
+++ templates/Menuitems/edit.php
@@ -8,9 +8,36 @@
 <h1 class="h3 mb-2 text-gray-800">Edit Menu Item</h1>
 <?= $this->Form->create($menuitem, ['type' => 'file', 'class' => 'text-gray-800']) ?>
 <?php
-echo $this->Form->control('menuitem_name', ['label' => 'Name', 'class' => 'form-control']);
-echo $this->Form->control('menuitem_desc', ['label' => 'Description', 'class' => 'form-control', 'maxlength' => '255', 'type' => 'textarea']);
-echo $this->Form->control('menuitem_price', ['label' => 'Price', 'class' => 'form-control', 'type' => 'number', 'min' => '2', 'max' => '50', 'maxlength' => '2', 'style' => 'margin-bottom: 10px']);
+echo $this->Form->control('menuitem_name', [
+    'label' => [
+        'text' => 'Name <span style="color: red;">*</span>',
+        'escape' => false
+    ],
+    'class' => 'form-control'
+]);
+
+echo $this->Form->control('menuitem_desc', [
+    'label' => [
+        'text' => 'Description <span style="color: red;">*</span>',
+        'escape' => false
+    ],
+    'type' => 'textarea',
+    'class' => 'form-control',
+    'maxlength' => '255'
+]);
+
+echo $this->Form->control('menuitem_price', [
+    'label' => [
+        'text' => 'Price <span style="color: red;">*</span>',
+        'escape' => false
+    ],
+    'class' => 'form-control',
+    'type' => 'number',
+    'min' => '2',
+    'max' => '50',
+    'maxlength' => '2',
+    'style' => 'margin-bottom: 10px'
+]);
 ?>
 <?= $this->Form->button(__('Submit'),['class'=>'btn btn-primary']) ?>
 <?= $this->Form->end() ?>
